12b292ab3e0551a37088afc7f152db861ffa0050
Dawsonkw <dawsonkw@gmail.com>
2024-05-09 21:27:03 -0600

    initial commit of Lets Roll app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1> /u/idontevenknowlol
+           Create a "Let's roll" app. User puts in name and time, it matches you with others. Or like an instant "i wanna now", and anyone can pick up ping with a "I can't let you get close".
+        Like a dating app, but for sweaty dudes.. 
+      </h1>
+    </>
+  )
+}
+
+export default App
